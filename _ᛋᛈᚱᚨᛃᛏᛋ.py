d3c90e6841b4bd29b2376207d5819614b75edde4
Jakub_Polák <polakj21@spstrutnov.cz>
2022-03-15 17:03:57 +0100

    Pálky... (pouze vertikální)

--- _ᛋᛈᚱᚨᛃᛏᛋ.py
+++ _ᛋᛈᚱᚨᛃᛏᛋ.py
@@ -2,19 +2,39 @@ import pygame
 pygame.init()
 
 #určení proměných
-width,height = 1000,600
-screen = pygame.display.set_mode((width,height))
+WIDTH,HEIGHT = 1000,600
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
 
 lighter = (224,248,208)
 light = (136,192,112)
 dark = (52,104,86)
 darker = (8,24,32)
 
+player_speed = 5
+
 #nehybné zdi
 class wall(pygame.sprite.Sprite):
     def __init__(self,start,width,height):
         super().__init__()
         self.image = pygame.Surface((width,height))
-        self.image.fill(dark)
+        self.image.fill(light)
         self.rect = self.image.get_rect()
         self.rect.topleft = start
+
+#vertikální hráč
+class ver_player(pygame.sprite.Sprite):
+    def __init__(self,pos,width,heigth,up,down):
+        super().__init__()
+        self.image = pygame.Surface((width,heigth))
+        self.image.fill(light)
+        self.rect = self.image.get_rect()
+        self.rect.center = (pos,HEIGHT//2)
+        self.up = up
+        self.down = down
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[self.up]:
+            self.rect.y -= player_speed
+        if keys[self.down]:
+            self.rect.y += player_speed
+        
--- ᛗᛖᛃᚾ.py
+++ ᛗᛖᛃᚾ.py
@@ -6,9 +6,19 @@ pygame.init()
 pygame.display.set_caption("ᛈᛟᚾᚷ᛬ᛈᚱᛟ᛬ᚲᛉᛏᛁᚱᛉᛁ᛬ᚺᚱᚨᚨᚲᛉᛖ")
 clock = pygame.time.Clock()
 
-#vyvolání zdí
-walls = pygame.sprite.Group(wall((0,0),width,80),wall((0,height-80),width,80))
+#vyvolání spriteů
+walls = pygame.sprite.Group(wall((0,0),WIDTH,60),wall((0,HEIGHT-60),WIDTH,60))
+players = pygame.sprite.Group(ver_player(95,25,80,pygame.K_w,pygame.K_s),ver_player(WIDTH-95,25,80,pygame.K_UP,pygame.K_DOWN))
 
+#koize
+def player_x_walls():
+    for wall in walls:
+        for player in players:
+            if wall.rect.collidepoint(player.rect.midtop):
+                player.rect.top = wall.rect.bottom
+            elif wall.rect.collidepoint(player.rect.midbottom):
+                player.rect.bottom = wall.rect.top
+            
 #main loop
 while True:
     #vypnutí
@@ -22,11 +32,14 @@ while True:
         pygame.quit()
         sys.exit()
     
-    
+    #update
+    players.update()
+    player_x_walls()
     
     #vykreslení
     screen.fill(darker)
     walls.draw(screen)
+    players.draw(screen)
     
     pygame.display.update()
     clock.tick(60)
